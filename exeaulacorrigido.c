409e1b6086ce6d1f9d58d4d4398c64691845a40e
Arthur Seus <arthur.seus.correa@gmail.com>
2022-08-29 08:59:53 -0300

    corrigido

--- exeaulacorrigido.c
+++ exeaulacorrigido.c
@@ -7,7 +7,6 @@ int main(void){
     void * pBuffer = malloc(1);
     void * imprimir;
     char * tipoNumero = malloc(1);
-    char * letraNumero, numero[2];
     int opc;
     int i, numeroTotal = 0, numeroInt = 0, numeroFloat = 0, numeroDouble = 0;
     float f;
@@ -20,7 +19,7 @@ int main(void){
     for (;;){
 
         printf("\n");
-        printf("deseja digitar...\n");
+        printf("Deseja digitar...\n");
         printf("Digite 1 [int] \n");
         printf("Digite 2 [float] \n");
         printf("Digite 3 [double] \n");
@@ -32,46 +31,55 @@ int main(void){
         if (opc == 1){
             printf("Digite o int: ");
             scanf("%d", &i);
-            pBuffer = realloc(pBuffer, (sizeof(int) * numeroInt) + (sizeof(float) * numeroFloat) + (sizeof(double) * numeroDouble));
+
+            pBuffer = realloc(pBuffer, (sizeof(int) * (numeroInt + 1)) + (sizeof(float) * numeroFloat) + (sizeof(double) * numeroDouble));
+
             lugarInt = (int *)(pBuffer + (sizeof(int) * numeroInt) + (sizeof(float) * numeroFloat) + (sizeof(double) * numeroDouble));
             *lugarInt = i;
+
             numeroInt++;
             numeroTotal++;
     
             tipoNumero = realloc(tipoNumero, sizeof(char) * numeroTotal);
             tipoNumero[numeroTotal - 1] = 'i'; //o problema foi ter usado "" ao invez de '' =(
-            printf("%c", tipoNumero[numeroTotal - 1]);
+            //printf("%c", tipoNumero[numeroTotal - 1]);
             
         }
         
         if (opc == 2){
             printf("Digite o float: ");
             scanf("%f", &f);
+
             pBuffer = realloc(pBuffer, (sizeof(int) * numeroInt)
-             + (sizeof(float) * numeroFloat) + (sizeof(double) * numeroDouble));
+             + (sizeof(float) * (numeroFloat + 1)) + (sizeof(double) * numeroDouble));
+
             lugarFloat = (float *)(pBuffer + (sizeof(int) * numeroInt) + (sizeof(float) * numeroFloat) + (sizeof(double) * numeroDouble));
             *lugarFloat = f;
+
             numeroFloat++;
             numeroTotal++;
 
             tipoNumero = realloc(tipoNumero, sizeof(char) * numeroTotal);
             tipoNumero[numeroTotal - 1] = 'f'; //o problema foi ter usado "" ao invez de '' =(
-            printf("%c", tipoNumero[numeroTotal - 1]);
+            //printf("%c", tipoNumero[numeroTotal - 1]);
         }
 
         if (opc == 3){
             printf("Digite o double: ");
             scanf("%lf", &d);
+
             pBuffer = realloc(pBuffer, (sizeof(int) * numeroInt)
-             + (sizeof(float) * numeroFloat) + (sizeof(double) * numeroDouble));
+             + (sizeof(float) * numeroFloat) + (sizeof(double) * (numeroDouble + 1)));
+
             lugarDouble = (double *)(pBuffer + (sizeof(int) * numeroInt) + (sizeof(float) * numeroFloat) + (sizeof(double) * numeroDouble));
             *lugarDouble = d;
+
             numeroDouble++; 
             numeroTotal++;
 
             tipoNumero = realloc(tipoNumero, sizeof(char) * numeroTotal);
             tipoNumero[numeroTotal - 1] = 'd'; //o problema foi ter usado "" ao invez de '' =(
-            printf("%c", tipoNumero[numeroTotal - 1]);
+            //printf("%c", tipoNumero[numeroTotal - 1]);
         }
 
         if (opc == 4){
@@ -109,11 +117,9 @@ int main(void){
                 }
                 else if (tipoNumero[j] == 'f'){
                     
-                    
                     imprimir = imprimir + sizeof(float);
                 }
                 else if (tipoNumero[j] == 'd'){
-                    
                     
                     imprimir = imprimir + sizeof(double);
                 }
@@ -125,7 +131,6 @@ int main(void){
                  
                 if (tipoNumero[j] == 'i'){
                     
-                    
                     imprimir = imprimir + sizeof(int);
                 }
                 else if (tipoNumero[j] == 'f'){
@@ -134,7 +139,6 @@ int main(void){
                     imprimir = imprimir + sizeof(float);
                 }
                 else if (tipoNumero[j] == 'd'){
-                    
                     
                     imprimir = imprimir + sizeof(double);
                 }
@@ -146,11 +150,9 @@ int main(void){
                  
                 if (tipoNumero[j] == 'i'){
                     
-                   
                     imprimir = imprimir + sizeof(int);
                 }
                 else if (tipoNumero[j] == 'f'){
-                    
                     
                     imprimir = imprimir + sizeof(float);
                 }
@@ -161,7 +163,6 @@ int main(void){
                 }
             }
             printf("\n");
-
 
             free(pBuffer);
             free(tipoNumero);
